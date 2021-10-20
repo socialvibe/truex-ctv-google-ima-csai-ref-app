e8c43cfd0ac0f23b9f15b75359da011370ac2381
blaak <blaak@ensemble.com>
2021-10-20 16:55:13 -0700

    WIP

--- src/simple/simple-video-controller.js
+++ src/simple/simple-video-controller.js
@@ -159,10 +159,10 @@ export class SimpleVideoController {
 
             // Normal scenario is to request a VAST VMAP ad playlist via a url.
             // For this demo application, we will use a canned xml response.
-            // adsRequest.adTagUrl = 'https://pubads.g.doubleclick.net/gampad/ads?' +
-            //     'sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&' +
-            //     'impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&' +
-            //     'cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator=';
+//            adsRequest.adTagUrl = 'https://pubads.g.doubleclick.net/gampad/ads?' +
+//                'sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&' +
+//                'impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&' +
+//                'cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator=';
             adsRequest.adResponse = require('../data/sample-ad-playlist.xml');
 
             // Specify the linear and nonlinear slot sizes. This helps the SDK to
