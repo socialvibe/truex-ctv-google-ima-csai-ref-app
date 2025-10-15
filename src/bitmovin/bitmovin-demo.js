08fd1d652525272ad26309f1641813bce5add961
Blagovest Dachev <blagovest.dachev@infillion.com>
2025-10-15 13:58:34 -0400

    PI-3454: Implement Bitmoving demo

---
+++ src/bitmovin/bitmovin-demo.js
@@ -0,0 +1,8 @@
+import { main } from '../main';
+import { BitmovinController } from "./bitmovin-controller";
+
+/**
+ * Demonstrates the use of the client-side IMA SDK in conjunction with the Bitmovin player, which manages the
+ * video player / ad player switching out of the box.
+ */
+main(BitmovinController);
